c05bf5b72d22298eed85f95faefe6464fe439b7f
Dylan Paiton <dpaiton@gmail.com>
2016-01-27 16:05:47 -0800

    runs minimize on new loss function, doesn't work
    minimize function fails.. not sure why

--- python/optimization_test/optimize.py
+++ python/optimization_test/optimize.py
@@ -2,6 +2,15 @@ import IPython
 import numpy as np
 from scipy.optimize import minimize
 
+def qDist(x, beta):
+    return np.exp(-beta * x) / np.array([np.sum(np.exp(-beta * x), axis=1),]*x.shape[1]).transpose()
+
+def pDist(x, beta):
+    return np.exp(-beta * x) / np.array([np.sum(np.exp(-beta * x), axis=0),]*x.shape[0])
+
+def entropy(prob, ax):
+    return - np.sum(np.multiply(prob, np.log(prob)), axis=ax)
+
 def optfn(x, num_batch, num_nodes, lamb, beta):
     '''
     Optimization callback for scipy.optimize.minimize()
@@ -10,58 +19,48 @@ def optfn(x, num_batch, num_nodes, lamb, beta):
     '''
 
     assert(x.shape[0] == num_batch * num_nodes)
-
     x = x.reshape(num_batch, num_nodes)
 
-    q_dist = np.exp(-beta * x) / np.sum(np.exp(-beta * x))
+    loss = np.sum(entropy(qDist(x, beta), 1)) - lamb * np.sum(entropy(pDist(x, beta), 0))
 
-    ent_fwd = -np.sum(np.sum(np.multiply(q_dist, np.log(q_dist)), axis=1), axis=0)
-    act_fwd = np.sum(np.power(np.sum(q_dist / num_batch, axis=0) - (1.0 / num_nodes), 2.0), axis=0)
+    qdiff = np.dot(qDist(x, beta), np.dot(x.transpose(), qDist(x, beta))) - np.multiply(qDist(x, beta), x)
+    pdiff = np.dot(pDist(x, beta), np.dot(x.transpose(), pDist(x, beta))) - np.multiply(pDist(x, beta), x)
+    grad = np.power(beta,2.0) * (qdiff - lamb * pdiff)
 
-    loss = ent_fwd + lamb * act_fwd
-
-    ent_tmp = np.array([np.sum(np.multiply(x, q_dist), axis=1),]*num_nodes).transpose() - x
-    ent_grad = np.sum(np.power(beta, 2.0) * np.multiply(q_dist, ent_tmp))
-
-    act_tmp1 = 2.0 * np.sum(np.sum(q_dist / num_batch, axis=0) - (1.0 / num_nodes), axis=0)
-    act_tmp2 = (-beta / num_batch) * np.sum(np.exp(-beta * x), axis=0)
-    act_grad = np.multiply(act_tmp1, act_tmp2)
-
-    IPython.embed()
-
-    grad = ent_grad + lamb * act_grad
-
-    return loss, grad
+    return loss, grad.flatten().astype(np.float64)
 
 def main(args):
     #jac:True means that the optfn returns gradient as well as loss
-    # requried for CG, BFGS< Newton-CG, L-BFGS-B, TNC, SLSQP, dogleg, trust-ncg
+    # requried for CG, BFGS, Newton-CG, L-BFGS-B, TNC, SLSQP, dogleg, trust-ncg
     minfn_args = {
         "args": (args["num_batch"], args["num_nodes"], args["lamb"], args["beta"]),
         "method": "L-BFGS-B", "jac": True,
-        "options": {"maxcor":8, "maxiter":args["n_iter"], "disp":args["verbose"]}
+        "options": {"maxcor": 8, "maxiter": args["n_iter"], "disp": args["verbose"]}
     }
 
-    res = []
-    for attempt in range(args["x0"].shape[0]):
-        res.append(minimize(optfn, args["x0"][attempt,:,:], **minfn_args).nit)
+    results = []
+    for example in range(args["x0"].shape[0]):
+        results.append(minimize(optfn, args["x0"][example,:,:], **minfn_args).nit)
 
     IPython.embed()
 
 if __name__ == "__main__":
-    n_iter = 1000
     verbose = True
+    n_iter = 1000
 
-    num_batch = 10
+    num_batch = 50
     num_nodes = 10
 
     lamb = 0.1
     beta = 1.0
 
-    num_attempts = 1
+    num_examples = 2
 
-    x0 = np.zeros((num_attempts, num_batch, num_nodes))
-    x0[0,:,:] = np.identity(num_nodes)
+    norm_mean = 0
+    norm_var = 1
+    x0 = np.zeros((num_examples, num_batch, num_nodes))
+    for example in range(num_examples):
+        x0[example,:,:] = np.random.normal(norm_mean, np.sqrt(norm_var), (num_batch, num_nodes))
 
     args = {
         "x0":x0,
@@ -73,5 +72,3 @@ if __name__ == "__main__":
         "verbose":verbose}
 
     main(args)
-
-
