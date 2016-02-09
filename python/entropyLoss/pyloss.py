99a3fa833833ed47d507aadfbc33149ef16da15c
Dylan Paiton <dpaiton@gmail.com>
2016-02-09 10:08:12 -0800

    loss function is now in a single caffe layer

--- python/entropyLoss/pyloss.py
+++ python/entropyLoss/pyloss.py
@@ -6,31 +6,38 @@ import theano.tensor as T
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plotIndiv(data, title_sup, prevFig=None):
+def plotIndiv(data, title_sup, num_imgs, prevFig=None):
     if prevFig[0] is None:
         figNo, figSubAxes = plt.subplots(np.ceil(np.sqrt(data.shape[1])).astype(np.int32), np.floor(np.sqrt(data.shape[1])).astype(np.int32))
         plt.suptitle(title_sup+' individual node activations')
+        barIds = []
+        for i in range(len(figSubAxes.flat)):
+            if i in range(data.shape[1]):
+                barIds.append(figSubAxes.flat[i].bar(range(0, num_imgs), data[0:num_imgs,i]))
+            figSubAxes.flat[i].tick_params(
+                axis='both',       # changes apply to the x-axis and y-axis
+                which='both',      # both major and minor ticks are affected
+                bottom='off',      # ticks along the bottom edge are off
+                top='off',         # ticks along the top edge are off
+                left='off',        # ticks along the left edge are off
+                right='off',       # ticks along the righ tedge are off
+                labelbottom='off', # labels along the bottom edge are off
+                labelleft='off')   # labels along the left edge are off
+            figSubAxes.flat[i].tick_params(
+                axis='y',          # changes apply to the x-axis and y-axis
+                labelleft='on',    # labels along the left edge are off
+                labelsize='8')     # font size of labels
     else:
-        (figNo, figSubAxes) = prevFig
-    for i in range(len(figSubAxes.flat)):
-        if i in range(data.shape[1]):
-            figSubAxes.flat[i].bar(range(0, data.shape[1]), data[i,:])
-        figSubAxes.flat[i].tick_params(
-            axis='both',       # changes apply to the x-axis and y-axis
-            which='both',      # both major and minor ticks are affected
-            bottom='off',      # ticks along the bottom edge are off
-            top='off',         # ticks along the top edge are off
-            left='off',        # ticks along the left edge are off
-            right='off',       # ticks along the righ tedge are off
-            labelbottom='off', # labels along the bottom edge are off
-            labelleft='off')   # labels along the left edge are off
-        #figSubAxes.flat[i].set_title(title_sup+' node activations for batch image number ' + str(i))
+        (figNo, figSubAxes, barIds) = prevFig
+        for i in range(len(figSubAxes.flat)):
+            if i in range(data.shape[1]):
+                for barId, h in zip(barIds[i], data[0:num_imgs, i]):
+                    barId.set_height(h)
     if prevFig[0] is None:
-        #figNo.set_tight_layout(True)
         figNo.show()
     else:
         figNo.canvas.draw()
-    return (figNo, figSubAxes)
+    return (figNo, figSubAxes, barIds)
 
 def plotBatch(data, title_sup, prevFig=None):
     if prevFig[0] is None:
@@ -53,6 +60,7 @@ class entropyLossLayer(caffe.Layer):
       self.lamb_ = np.float(layer_params['lambda'])
       self.figNo = [None, None, None] # for debugging
       self.figSubAxes = [None, None, None] # for debugging
+      self.barId = [None, None] # for debugging
 
    def reshape(self, bottom, top):
       #inputs
@@ -79,12 +87,12 @@ class entropyLossLayer(caffe.Layer):
    def forward(self, bottom, top):
       fwd_out = self.forward_fn(bottom[0].data, self.beta_, self.alpha_, self.lamb_) 
       top[0].data[...] = fwd_out[0]
-      (self.figNo[0], self.figSubAxes[0]) = plotIndiv(bottom[0].data, 'data', (self.figNo[0], self.figSubAxes[0]))
-      (self.figNo[1], self.figSubAxes[1]) = plotIndiv(fwd_out[1], 'q prob', (self.figNo[1], self.figSubAxes[1]))
+      (self.figNo[0], self.figSubAxes[0], self.barId[0]) = plotIndiv(bottom[0].data, 'data', 15, (self.figNo[0], self.figSubAxes[0], self.barId[0]))
+      (self.figNo[1], self.figSubAxes[1], self.barId[1]) = plotIndiv(fwd_out[1], 'q prob', 15, (self.figNo[1], self.figSubAxes[1], self.barId[1]))
       (self.figNo[2], self.figSubAxes[2]) = plotBatch(fwd_out[2], 'p prob', (self.figNo[2], self.figSubAxes[2]))
       print '-------\ntot loss: ' + str(fwd_out[0])
-      print 'q loss: ' + str(self.forward_fn(bottom[0].data, self.beta_, 1.0, 0.0)[0])
-      print 'p loss: ' + str(self.forward_fn(bottom[0].data, self.beta_, 0.0, 1.0)[0])
+      print 'q loss: ' + str(self.forward_fn(bottom[0].data, self.beta_, self.alpha_*1.0, 0.0)[0])
+      print 'p loss: ' + str(self.forward_fn(bottom[0].data, self.beta_, 0.0, self.lamb_*1.0)[0])
 
    def backward(self, top, propagate_down, bottom):
       bottom[0].diff[...] = self.grad_fn(bottom[0].data, self.beta_, self.lamb_, self.alpha_)
