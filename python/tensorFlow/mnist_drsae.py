f51d29ba1cab296a2357f1ded4f7ab269d5473da
Dylan Paiton <dpaiton@gmail.com>
2016-04-12 09:49:03 -0700

    specify cpu or gpu, bug fixes

--- python/tensorFlow/mnist_drsae.py
+++ python/tensorFlow/mnist_drsae.py
@@ -16,12 +16,9 @@ batch_ = 60            # Number of images in a batch
 learning_rate_ = 0.05  # Learning rate for SGD
 num_steps_ = 11        # Number of time steps for enoding
 num_trials_ = 1000000  # Number of batches to learn weights
-train_display_ = 1000  # How often to update training stats outputs
+train_display_ = 100   # How often to update training stats outputs
 val_display_ = -1      # How often to update validation stats outputs
 checkpoint_ = 50000    # How often to checkpoint weights
-
-# Interactive session allows us to enter IPython for analysis
-sess = tf.InteractiveSession()
 
 ## Input data
 dataset = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -109,65 +106,67 @@ b_prev_fig = None
 init_op = tf.initialize_all_variables()
 saver = tf.train.Saver(max_to_keep=int(np.float32(num_trials_)/np.float32(checkpoint_)))
 
-sess.run(init_op)
-for batch_idx in range(num_trials_):
-    ## Load data
-    batch = dataset.train.next_batch(batch_)
-    input_image = hf.normalize_image(batch[0], divide_l2=True)
+with tf.Session() as sess:
+    with tf.device('/cpu:0'): # specify hardware, could be cpu:0, gpu:0, gpu:1, etc
+        sess.run(init_op)
+        for batch_idx in range(num_trials_):
+            ## Load data
+            batch = dataset.train.next_batch(batch_)
+            input_image = hf.normalize_image(batch[0], divide_l2=True)
 
-    """
-    Need to normalize weight matrices
-    per Rolfe & Lecun (2013)
-     Magnitude of rows of E are bounded by 1.25/num_steps_
-        ***Changing to be l2 norm of each basis vector in E is 1 (from rozell et al 2008)
-     Magnitude of columns of D are bounded by 1
-     Magnitude of rows of C are bounded by 5
-    """
-    #E = hf.normalize_rows(E, 1.25/np.float32(num_steps_))
-    E = hf.l2_normalize_rows(E)
-    D_W = hf.normalize_cols(D_W, 1.0)
-    C = hf.normalize_rows(C, 5.0)
+            """
+            Need to normalize weight matrices
+            per Rolfe & Lecun (2013)
+             Magnitude of rows of E are bounded by 1.25/num_steps_
+                ***Changing to be l2 norm of each basis vector in E is 1 (from rozell et al 2008)
+             Magnitude of columns of D are bounded by 1
+             Magnitude of rows of C are bounded by 5
+            """
+            #E = hf.normalize_rows(E, 1.25/np.float32(num_steps_))
+            E = hf.l2_normalize_rows(E)
+            D_W = hf.normalize_cols(D_W, 1.0)
+            C = hf.normalize_rows(C, 5.0)
 
-    ## First find image code, z
-    z.assign(zeros)
-    for t in range(int(num_steps_ - 1)):
-        step_z.run({x:input_image})
+            ## First find image code, z
+            z.assign(zeros)
+            for t in range(int(num_steps_ - 1)):
+                step_z.run({x:input_image})
 
-    ## Use converged z to compute loss & update weights
-    train_step.run({x:input_image, y:batch[1], gamma:0.0})
+            ## Use converged z to compute loss & update weights
+            train_step.run({x:input_image, y:batch[1], gamma:0.0})
 
-    if train_display != -1 and batch_idx % train_display_ == 0:
-        train_accuracy = accuracy.eval({x:input_image, y:batch[1]})
-        print('Completed batch number %g out of %g'%(batch_idx, num_trials_))
-        print('\ttrain accuracy:\t\t%g'%(train_accuracy))
-        print('\teuclidean loss:\t\t%g'%(euclidean_loss.eval({x:input_image})))
-        print('\tsparse loss:\t\t%g'%(sparse_loss.eval({x:input_image})))
-        print('\tcross-entropy loss:\t%g'%(cross_entropy_loss.eval({x:input_image, y:batch[1]})))
-        print('\talpha_1 value:\t\t%g'%(alpha_1.eval()))
-        print('\talpha_2 value:\t\t%g'%(alpha_2.eval()))
+            if train_display_ != -1 and batch_idx % train_display_ == 0:
+                train_accuracy = accuracy.eval({x:input_image, y:batch[1]})
+                print('Completed batch number %g out of %g'%(batch_idx, num_trials_))
+                print('\ttrain accuracy:\t\t%g'%(train_accuracy))
+                print('\teuclidean loss:\t\t%g'%(euclidean_loss.eval({x:input_image})))
+                print('\tsparse loss:\t\t%g'%(sparse_loss.eval({x:input_image})))
+                print('\tcross-entropy loss:\t%g'%(cross_entropy_loss.eval({x:input_image, y:batch[1]})))
+                print('\talpha_1 value:\t\t%g'%(alpha_1.eval()))
+                print('\talpha_2 value:\t\t%g'%(alpha_2.eval()))
 
-    if val_display_ != -1 and batch_idx % val_display_ == 0:
-        e_prev_fig = hf.display_data(E.eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
-            title='Encoding matrix at trial number '+str(batch_idx), prev_fig=e_prev_fig)
-        d_prev_fig = hf.display_data(tf.transpose(D).eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
-            title='Decoding matrix at trial number '+str(batch_idx), prev_fig=d_prev_fig)
-        s_prev_fig = hf.display_data(S.eval(),
-            title='Explaining-away matrix at trial number '+str(batch_idx), prev_fig=s_prev_fig)
-        c_prev_fig = hf.display_data(C.eval().reshape(l_, int(np.sqrt(n_)), int(np.sqrt(n_))),
-            title='Classification matrix at trial number '+str(batch_idx), prev_fig=c_prev_fig)
-        b_prev_fig = hf.display_data(b.eval().reshape(int(np.sqrt(n_)), int(np.sqrt(n_))),
-            title='Bias at trial number '+str(batch_idx)+'\nEach pixel represents the bias for a neuron',
-            prev_fig=b_prev_fig)
-        batch = dataset.validation.next_batch(batch_)
-        val_accuracy = accuracy.eval({x:input_image, y:batch[1]})
-        print('---validation accuracy %g'%(val_accuracy))
+            if val_display_ != -1 and batch_idx % val_display_ == 0:
+                e_prev_fig = hf.display_data(E.eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
+                    title='Encoding matrix at trial number '+str(batch_idx), prev_fig=e_prev_fig)
+                d_prev_fig = hf.display_data(tf.transpose(D).eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
+                    title='Decoding matrix at trial number '+str(batch_idx), prev_fig=d_prev_fig)
+                s_prev_fig = hf.display_data(S.eval(),
+                    title='Explaining-away matrix at trial number '+str(batch_idx), prev_fig=s_prev_fig)
+                c_prev_fig = hf.display_data(C.eval().reshape(l_, int(np.sqrt(n_)), int(np.sqrt(n_))),
+                    title='Classification matrix at trial number '+str(batch_idx), prev_fig=c_prev_fig)
+                b_prev_fig = hf.display_data(b.eval().reshape(int(np.sqrt(n_)), int(np.sqrt(n_))),
+                    title='Bias at trial number '+str(batch_idx)+'\nEach pixel represents the bias for a neuron',
+                    prev_fig=b_prev_fig)
+                batch = dataset.validation.next_batch(batch_)
+                val_accuracy = accuracy.eval({x:input_image, y:batch[1]})
+                print('---validation accuracy %g'%(val_accuracy))
 
-    if checkpoint_ != -1 and batch_idx % checkpoint_ == 0:
-        save_path = saver.save(sess=sess,
-            save_path='checkpoints/drsae_model',
-            global_step=batch_idx,
-            latest_filename='latest.log')
-        print("Model saved in file %s"%save_path)
+            if checkpoint_ != -1 and batch_idx % checkpoint_ == 0:
+                save_path = saver.save(sess=sess,
+                    save_path='checkpoints/drsae_model',
+                    global_step=batch_idx,
+                    latest_filename='latest.log')
+                print("Model saved in file %s"%save_path)
 
 print('Model has finished learning.')
 IPython.embed()
