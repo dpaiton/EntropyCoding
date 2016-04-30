31022c9fe394a4ea98f31cec79ec2101d9235c0b
Dylan Paiton <dpaiton@gmail.com>
2016-04-30 11:55:44 -0700

    improved readability and bug fixes
    Encoding matrix was not normalized correctly
    renamed some variables for readability with tensorboard
    model still does not converge correctly

--- python/tensorFlow/helper_functions.py
+++ python/tensorFlow/helper_functions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import IPython
 
 """
-Display input data as an image
+Display input data as an image with reshaping
 Outputs:
     fig_no: index for figure call
     sub_axis: index for subplot call
@@ -14,7 +14,7 @@ Inpus:
     title: string for title of figure
     prev_fig: tuple containing (fig_no, sub_axis, axis_image) from previous display_data() call
 """
-def display_data(data, title='', prev_fig=None):
+def display_data_tiled(data, title='', prev_fig=None):
     # normalize input
     data = (data - data.min()) / (data.max() - data.min())
 
@@ -33,15 +33,39 @@ def display_data(data, title='', prev_fig=None):
     if prev_fig is None:
         fig_no, sub_axis = plt.subplots(1)
         axis_image = sub_axis.imshow(data, cmap='Greys', interpolation='nearest')
+        cbar = fig_no.colorbar(axis_image)
     else:
         (fig_no, sub_axis, axis_image) = prev_fig
         axis_image.set_data(data)
+        axis_image.autoscale()
 
-    for axis in fig_no.axes:
-        axis.get_yaxis().set_visible(False)
-        axis.get_xaxis().set_visible(False)
+    #for axis in fig_no.axes:
+    #    axis.get_yaxis().set_visible(False)
+    #    axis.get_xaxis().set_visible(False)
 
     fig_no.suptitle(title)
+    if prev_fig is None:
+        fig_no.show()
+    else:
+        fig_no.canvas.draw()
+        
+    return (fig_no, sub_axis, axis_image)
+
+"""
+Display input data as an image without reshaping
+"""
+def display_data(data, title='', prev_fig=None):
+    if prev_fig is None:
+        fig_no, sub_axis = plt.subplots(1)
+        axis_image = sub_axis.imshow(data, cmap='Greys', interpolation='nearest')
+        cbar = fig_no.colorbar(axis_image)
+    else:
+        (fig_no, sub_axis, axis_image) = prev_fig
+        axis_image.set_data(data)
+        axis_image.autoscale()
+
+    fig_no.suptitle(title)
+
     if prev_fig is None:
         fig_no.show()
     else:
@@ -66,69 +90,82 @@ def normalize_image(img, divide_l2=False):
     return output
 
 """ 
-Independently normalize the columns of matrix to be bounded by bound
+Independently normalize the columns of tensor to be bounded by bound
 
 Outputs:
-    column normalized matrix
+    bound_norm_mat : column normalized tensor
 
 Inputs:
-    matrix : tensorflow tensor
+    tensor : 2-D tensorflow tensor
     bound  : flaot32
 """
-def normalize_cols(matrix, bound):
-    num_rows = int(matrix.get_shape()[0])
+def normalize_cols(tensor, bound):
+    num_rows = int(tensor.get_shape()[0])
     ones_mat = tf.constant(np.ones([num_rows, 1], dtype=np.float32))
-    max_mat = tf.matmul(ones_mat, tf.expand_dims(tf.reduce_max(matrix, reduction_indices=0), 0))
-    min_mat = tf.matmul(ones_mat, tf.expand_dims(tf.reduce_min(matrix, reduction_indices=0), 0))
-    norm_mat = tf.truediv(tf.sub(matrix, min_mat), tf.sub(max_mat,min_mat))
+    max_mat = tf.matmul(ones_mat, tf.expand_dims(tf.reduce_max(tensor, reduction_indices=0), 0))
+    min_mat = tf.matmul(ones_mat, tf.expand_dims(tf.reduce_min(tensor, reduction_indices=0), 0))
+    norm_mat = tf.truediv(tf.sub(tensor, min_mat), tf.sub(max_mat, min_mat))
     bound_norm_mat = tf.mul(norm_mat, tf.constant(np.float32(bound)))
     return bound_norm_mat
 
 """ 
-Independently normalize the rows of matrix to be bounded by bound
+Independently normalize the rows of tensor to be bounded by bound
 
 Outputs:
-    row normalized matrix
+    bound_norm_mat : row normalized tensor
 
 Inputs:
-    matrix : tensorflow tensor
+    tensor : 2-D tensorflow tensor
     bound  : flaot32
 """
-def normalize_rows(matrix, bound):
-    num_cols = int(matrix.get_shape()[1])
+def normalize_rows(tensor, bound):
+    num_cols = int(tensor.get_shape()[1])
     ones_mat = tf.constant(np.ones([1, num_cols], dtype=np.float32))
-    max_mat = tf.matmul(tf.expand_dims(tf.reduce_max(matrix, reduction_indices=1), 1), ones_mat)
-    min_mat = tf.matmul(tf.expand_dims(tf.reduce_min(matrix, reduction_indices=1), 1), ones_mat)
-    norm_mat = tf.truediv(tf.sub(matrix, min_mat), tf.sub(max_mat,min_mat))
+    max_mat = tf.matmul(tf.expand_dims(tf.reduce_max(tensor, reduction_indices=1), 1), ones_mat)
+    min_mat = tf.matmul(tf.expand_dims(tf.reduce_min(tensor, reduction_indices=1), 1), ones_mat)
+    norm_mat = tf.truediv(tf.sub(tensor, min_mat), tf.sub(max_mat, min_mat))
     bound_norm_mat = tf.mul(norm_mat, tf.constant(np.float32(bound)))
     return bound_norm_mat
 
 """ 
-Normalize the columns of matrix to unit norm
-For matrix W of dimensions (m,n),
+Normalize the columns of tensor to unit norm
+For tensor W of dimensions (m,n),
 set the l2 norm of each of the n columns to 1
 ||W_n||_2 = 1 for all n
 
 Outputs:
-    column normalized matrix
+    column normalized tensor
 
 Inputs:
-    matrix : tensorflow tensor
+    tensor : 2-D tensorflow tensor
 """
-def l2_normalize_cols(matrix):
-    return tf.matmul(matrix, tf.diag(1.0/tf.sqrt(tf.reduce_sum(tf.pow(matrix, 2.0), reduction_indices=0))))
+def l2_normalize_cols(tensor):
+    return tf.matmul(tensor, tf.diag(1.0/tf.sqrt(tf.reduce_sum(tf.pow(tensor, 2.0), reduction_indices=0))))
 
 """ 
-Normalize the rows of matrix to unit norm
-For matrix W of diminsions (m, n),
+Normalize the rows of tensor to unit norm
+For tensor W of diminsions (m, n),
 set the l2 norm of each of the m rows to 1
 ||W_m||_2 = 1 for all m
 
 Outputs:
-    row normalized matrix
+    row normalized tensor
 
 Inputs:
-    matrix : tensorflow tensor
+    tensor : 2-D tensorflow tensor
 """
-def l2_normalize_rows(matrix):
-    return tf.matmul(tf.diag(1.0/tf.sqrt(tf.reduce_sum(tf.pow(matrix, 2.0), reduction_indices=1))), matrix)
+def l2_normalize_rows(tensor):
+    return tf.matmul(tf.diag(1.0/tf.sqrt(tf.reduce_sum(tf.pow(tensor, 2.0), reduction_indices=1))), tensor)
+
+""" 
+Normalize input tensor to be between 0 and bound
+Outputs:
+    normalized tensor
+Inputs:
+    tensor : tensorflow tensor
+"""
+def normalize_tensor(tensor, bound):
+    tensor_max = tf.reduce_max(tensor)
+    tensor_min = tf.reduce_min(tensor)
+    norm_tensor = tf.truediv(tf.sub(tensor, tensor_min), tf.sub(tensor_max, tensor_min))
+    return tf.mul(norm_tensor, tf.constant(np.float32(bound)))
--- python/tensorFlow/mnist_drsae.py
+++ python/tensorFlow/mnist_drsae.py
@@ -19,22 +19,20 @@ m_ = 784               # Number of pixels
 n_ = 484               # Number of hidden units
 l_ = 10                # Number of categories
 batch_ = 60            # Number of images in a batch
-train_display_ = 2     # How often to update training stats outputs
+train_display_ = 5     # How often to update training stats outputs
 val_display_ = -1      # How often to update validation stats outputs
 display_plots_ = True  # If True, plots will display on train_display_ intervals
 device_ = "/cpu:0"     # Specify hardware; can be "/cpu:0", "/gpu:0", "/gpu:1"
 
 ## Checkpointing
-checkpoint_ = 10000   # How often to checkpoint weights. -1 for no checkpointing
-checkpoint_write_prefix_ = "v0.01"
+# Writing
+checkpoint_ = 5000    # How often to checkpoint weights. -1 for no checkpointing
+checkpoint_write_prefix_ = "v0.02"
+# Reading
 load_checkpoint_ = False
-checkpoint_trial_ = 10000
-#checkpoint_read_prefix_ = "unsup_encode_0.001sparse"
-#checkpoint_read_prefix_ = "unsup_encode_b_s_d_0.001sparse"
-#checkpoint_read_prefix_ = "unsup_encode_b_s_d_0.01sparse" # finished 10000
-#checkpoint_read_prefix_ = "sup_e_b_s_d_c_l0.01_g0.1" # finished 15000 iterations
-#checkpoint_read_prefix_ = "sup_e_b_s_d_c_l0.01_g0.2" # finished 60000 iterations - nan after 57000 (96% train acc)
-checkpoint_read_prefix_ = "v0.01_s0"
+global_batch_index_ = 20000
+checkpoint_sched_no_ = 1
+checkpoint_read_prefix_ = "v0.01"
 
 tf.set_random_seed(1234567890)
 
@@ -60,26 +58,26 @@ with tf.name_scope("Parameters") as scope:
 ## Learned variables - to be trained with backprop
 # Encoding weights
 with tf.name_scope("encode_weights") as scope:
-    E = tf.Variable(hf.l2_normalize_rows(tf.truncated_normal([m_, n_], mean=0.0,
-        stddev=1.00, dtype=tf.float32, name="E_init")), trainable=True, name="encode_weights")
+    E = tf.Variable(hf.l2_normalize_cols(tf.truncated_normal([m_, n_], mean=0.0, stddev=np.sqrt(1.0),
+        dtype=tf.float32, name="E_init")), trainable=True, name="encode_weights")
 
 # Decoding weights
 with tf.name_scope("decode_weights") as scope:
     alpha_1 = tf.Variable(np.float32(1.0), trainable=True, name="alpha_1")
     D_W = tf.Variable(tf.truncated_normal([n_, m_], mean=0.0, stddev=np.sqrt(1e-4),
         dtype=tf.float32, name="D_W_init"), trainable=True, name="D_W")
-    D = tf.sigmoid(alpha_1) * tf.transpose(E) + (1-tf.sigmoid(alpha_1)) * D_W
+    D = tf.sigmoid(alpha_1) * tf.transpose(E) + (1.0 - tf.sigmoid(alpha_1)) * D_W
 
 # Explaining away weights
 with tf.name_scope("recurrent_weights") as scope:
     alpha_2 = tf.Variable(np.float32(1.0), trainable=True, name="alpha_2")
     S_W = tf.Variable(tf.truncated_normal([n_, n_], mean=0.0, stddev=np.sqrt(1e-4),
         dtype=tf.float32, name="S_W_init"), trainable=True, name="S_W")
-    S = tf.sigmoid(alpha_2) * (tf.matmul(tf.transpose(E), E, name="Gramian") - identity_mat) + (1 - tf.sigmoid(alpha_2)) * S_W
+    S = tf.sigmoid(alpha_2) * (identity_mat - tf.matmul(tf.transpose(E), E, name="Gramian")) + (1.0 - tf.sigmoid(alpha_2)) * S_W
 
 # Classification matrix
 with tf.name_scope("classification_weights") as scope:
-    C = tf.Variable(tf.truncated_normal([n_, l_], mean=0.0, stddev=1.0,
+    C = tf.Variable(tf.truncated_normal([n_, l_], mean=0.0, stddev=np.sqrt(1.0),
         dtype=tf.float32, name="C_init"), trainable=True, name="C")
 
 # Bias
@@ -88,18 +86,17 @@ b = tf.Variable(np.zeros([1, n_], dtype=np.float32), trainable=True, name="bias"
 ## Dynamic variables - not to be trained with backrpop
 z = tf.Variable(np.zeros([batch_, n_], dtype=np.float32), trainable=False, name="z")
 
-# Discretized update rule: z(t+1) = ReLU(eta * (x E - z(t) * (S-I) - b))
+# Discretized update rule: z(t+1) = ReLU(eta * (x * E + z(t) * S - b))
 with tf.name_scope("update_z") as scope:
-    zT = tf.nn.relu(tf.matmul(x, E, name="encoding_transform") - \
-        tf.matmul(z, S, name="explaining_away") - \
+    zT = tf.nn.relu(tf.matmul(x, E, name="encoding_transform") +\
+        tf.matmul(z, S, name="explaining_away") -\
         tf.matmul(tf.constant(np.ones([batch_, 1], dtype=np.float32)), b, name="bias_replication"),
-        name="zT")
+        name="update_z")
     step_z = tf.group(z.assign(eta * zT))
 
 # Normalized z for classification
 with tf.name_scope("normalize_z") as scope:
-    norm_z = tf.truediv(zT, tf.sqrt(tf.reduce_sum(tf.pow(zT, 2.0)))+1e-9) # added epsilon to prevent divide by 0
-    #norm_z = tf.matmul(zT, tf.diag(1.0/(tf.sqrt(tf.reduce_sum(tf.pow(zT, 2.0), reduction_indices=0))+1e-9)))
+    norm_z = tf.truediv(zT, tf.sqrt(tf.reduce_sum(tf.pow(zT, 2.0))) + 1e-10)
 
 ## Network outputs
 with tf.name_scope("output") as scope:
@@ -118,23 +115,22 @@ with tf.name_scope("loss") as scope:
         unsupervised_loss = euclidean_loss + sparse_loss
     with tf.name_scope("supervised_loss"):
         with tf.name_scope("cross_entropy_loss"):
-            cross_entropy_loss = gamma * -tf.reduce_sum(y * tf.log(tf.clip_by_value(y_, 1e-10, 1.0))) # prevent 0log(0)
+            cross_entropy_loss = gamma * -tf.reduce_sum(y * tf.log(tf.clip_by_value(y_, 1e-10, 1.0)))
         supervised_loss = cross_entropy_loss
     total_loss = unsupervised_loss + supervised_loss
 
+## Load in scheduler
+schedules = scheduler.schedule().blocks
+
 ## Weight update method
-# ADADELTA updates according to a learning rate that is scaled per
-# parameter and is proportional to the sum of the squares of the
-# gradients of past time steps within a decaying average window.
-# General overview of learning methods can be found at this blog post:
-# http://sebastianruder.com/optimizing-gradient-descent/index.html
-decay_rate_ = 0.9
-epsilon_ = 1e-2
-learning_rate_ = 1.0
-#train_step = tf.train.AdadeltaOptimizer(lr, decay_rate_, epsilon_,
-#    name='grad_update').minimize(total_loss, var_list=[E, alpha_1, D_W, alpha_2, S_W, b, C])
-train_step = tf.train.GradientDescentOptimizer(lr,
-    name='grad_update').minimize(total_loss, var_list=[E, alpha_1, D_W, alpha_2, S_W, b, C])
+#beta_1_ = 0.9
+#beta_2_ = 0.999
+#epsilon_ = 1e-7
+#train_steps = [tf.train.AdamOptimizer(lr, beta_1_, beta_2_, epsilon_,
+#    name="adam_update").minimize(total_loss,
+#    var_list=[E, alpha_1, D_W, alpha_2, S_W, b, C]) for sch in range(len(schedules))]
+train_steps = [tf.train.GradientDescentOptimizer(lr, name="grad_desc_update").minimize(total_loss,
+    var_list=[E, alpha_1, D_W, alpha_2, S_W, b, C]) for sch in range(len(schedules))]
 
 ## Accuracy functions
 with tf.name_scope("accuracy_calculation") as scope:
@@ -149,13 +145,11 @@ d_prev_fig = None
 s_prev_fig = None
 c_prev_fig = None
 b_prev_fig = None
+z_prev_fig = None
 recon_prev_fig = None
 
 ## Initialization
 init_op = tf.initialize_all_variables()
-
-## Load in scheduler
-schedules = scheduler.schedule().blocks
 
 if checkpoint_ != -1:
     saver = tf.train.Saver()
@@ -167,7 +161,8 @@ with tf.Session() as sess:
         sess.run(init_op)
 
         if load_checkpoint_:
-            saver.restore(sess, "checkpoints/"+checkpoint_read_prefix_+"_drsae_model-"+str(checkpoint_trial_))
+            saver.restore(sess,
+                "checkpoints/"+checkpoint_read_prefix_+"_s"+str(checkpoint_sched_no_)+"_drsae_model-"+str(global_batch_index_))
 
         ## Write graph to text file
         tf.train.write_graph(sess.graph_def, "checkpoints", "drsae_graph.pb", False)
@@ -175,6 +170,9 @@ with tf.Session() as sess:
 
         global_batch_timer = 0
         for sched_no, schedule in enumerate(schedules):
+            if load_checkpoint_ and sched_no < checkpoint_sched_no_:
+                global_batch_timer += schedule["num_batches"]-1
+                continue
             print("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
             print("Beginning schedule:")
             print(schedule)
@@ -186,35 +184,28 @@ with tf.Session() as sess:
             num_steps_ = schedule["num_steps"]         # Number of time steps for enoding
             num_batches_ = schedule["num_batches"]     # Number of batches to learn weights
 
-            ## Checkpoint loading
-            if load_checkpoint_:
-                trial_start = checkpoint_trial_
-                assert(trial_start < num_batches_)
-            else:
-                trial_start = 0
-
             for batch_idx in range(num_batches_):
                 ## Load data
                 batch = dataset.train.next_batch(batch_)
-                global_batch_timer += 1
-                if batch_idx < trial_start:
+                if load_checkpoint_ and batch_idx < (global_batch_index_ - global_batch_timer):
                     #TODO: advance batch pointer so this doesn't have to be done in a loop
                     continue
 
                 input_image = hf.normalize_image(batch[0], divide_l2=False)
 
                 ## Normalize weight matrices
-                E.assign(hf.l2_normalize_rows(E))
-                D_W.assign(hf.normalize_cols(D_W, 1.0))
-                C.assign(hf.normalize_rows(C, 1.0))
+                E.assign(hf.l2_normalize_cols(E))
+                #D_W.assign(hf.l2_normalize_rows(D_W))
+                #S_W.assign(hf.normalize_tensor(S_W, 1.0))
+                C.assign(hf.normalize_cols(C, 1.0))
 
                 ## First find image code, z
                 z.assign(zeros)
                 for t in range(int(num_steps_)):
                     step_z.run({x:input_image, eta:eta_})
 
-                ## Use converged zT to compute loss & update weights
-                train_step.run({\
+                ## Use converged z to compute loss & update weights
+                train_steps[sched_no].run({\
                     x:input_image,
                     y:batch[1],
                     lr:learning_rate_,
@@ -224,30 +215,35 @@ with tf.Session() as sess:
                 if train_display_ != -1 and global_batch_timer % train_display_ == 0:
                     train_accuracy = accuracy.eval({x:input_image, y:batch[1]})
                     if display_plots_:
-                        c_prev_fig = hf.display_data(C.eval().reshape(l_, int(np.sqrt(n_)), int(np.sqrt(n_))),
+                        c_prev_fig = hf.display_data_tiled(tf.transpose(C).eval().reshape(l_, int(np.sqrt(n_)), int(np.sqrt(n_))),
                             title="Classification matrix at trial number "+str(global_batch_timer), prev_fig=c_prev_fig)
-                        b_prev_fig = hf.display_data(b.eval().reshape(int(np.sqrt(n_)), int(np.sqrt(n_))),
+                        z_prev_fig = hf.display_data(z.eval(), title="Z values", prev_fig=z_prev_fig)
+                        b_prev_fig = hf.display_data_tiled(b.eval().reshape(int(np.sqrt(n_)), int(np.sqrt(n_))),
                             title="Bias at trial number "+str(global_batch_timer)+"\nEach pixel represents the bias for a neuron",
                             prev_fig=b_prev_fig)
-                        s_prev_fig = hf.display_data(S.eval(),
+                        s_prev_fig = hf.display_data_tiled(S.eval(),
                             title="Explaining-away matrix at trial number "+str(batch_idx), prev_fig=s_prev_fig)
-                        d_prev_fig = hf.display_data(tf.transpose(D).eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
+                        d_prev_fig = hf.display_data_tiled(D.eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
                             title="Decoding matrix at trial number "+str(batch_idx), prev_fig=d_prev_fig)
-                        e_prev_fig = hf.display_data(E.eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
+                        e_prev_fig = hf.display_data_tiled(tf.transpose(E).eval().reshape(n_, int(np.sqrt(m_)), int(np.sqrt(m_))),
                             title="Encoding matrix at trial number "+str(batch_idx), prev_fig=e_prev_fig)
-                        recon_prev_fig = hf.display_data(
+                        recon_prev_fig = hf.display_data_tiled(
                             x_.eval({x:input_image}).reshape(batch_, int(np.sqrt(m_)), int(np.sqrt(m_))),
                             title="Reconstructions for trial number "+str(batch_idx), prev_fig=recon_prev_fig)
 
-                    print("\nCompleted batch number %g out of %g"%(batch_idx, num_batches_))
-                    print("\ttrain accuracy:\t\t%g"%(train_accuracy))
+                    perc_active = 100*np.count_nonzero(z.eval())/np.float32(np.size(z.eval()))
+                    print("\nGlobal iteration number is %g"%global_batch_timer)
+                    print("\tCompleted batch number %g out of %g in current schedule"%(batch_idx, num_batches_))
+                    print("\tpercent active:\t\t%g"%(perc_active))
                     print("\talpha_1 value:\t\t%g"%(1.0/(1.0+np.exp(-alpha_1.eval()))))
                     print("\talpha_2 value:\t\t%g"%(1.0/(1.0+np.exp(-alpha_2.eval()))))
                     print("\teuclidean loss:\t\t%g"%(euclidean_loss.eval({x:input_image})))
-                    print("\tsparse loss:\t\t%g"%(sparse_loss.eval({x:input_image, lamb:lambda_})))
+                    #print("\tsparse loss:\t\t%g"%(sparse_loss.eval({lamb:lambda_})))
+                    print("\tsparse loss:\t\t%g"%(sparse_loss.eval({x:input_image,lamb:lambda_})))
                     print("\tunsupervised loss:\t%g"%(unsupervised_loss.eval({x:input_image, lamb:lambda_})))
                     print("\tcross-entropy loss:\t%g"%(cross_entropy_loss.eval({x:input_image, y:batch[1], gamma:gamma_})))
                     print("\tsupervised loss:\t%g"%(supervised_loss.eval({x:input_image, y:batch[1], gamma:gamma_})))
+                    print("\ttrain accuracy:\t\t%g"%(train_accuracy))
 
                 if val_display_ != -1 and global_batch_timer % val_display_ == 0:
                     val_batch = dataset.validation.next_batch(batch_)
@@ -262,6 +258,8 @@ with tf.Session() as sess:
                         global_step=global_batch_timer,
                         latest_filename=checkpoint_write_prefix_+"_latest.log")
                     print("Model saved in file %s"%save_path)
+
+                global_batch_timer += 1
 
         print("Model has finished learning schedule.")
         IPython.embed()
