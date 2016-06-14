ae6bb0b9e76a0d8a5e42df08995ab92e9c6daefe
dpaiton <dpaiton@gmail.com>
2016-06-14 12:04:26 -0700

    extended run length

--- python/tensorFlow/lca_schedule.py
+++ python/tensorFlow/lca_schedule.py
@@ -6,25 +6,32 @@ num_steps      # Number of time steps for enoding
 num_batches    # Number of batches to learn weights
 """
 class schedule(object):
-    def __init__(self):
-        self.blocks = [ \
-            {"prefix":"unsupervised",
-            "lambda":0.1,
-            "gamma":0.0,
-            "learning_rate":0.001,
-            "num_steps":50,
-            "num_batches":10001},
-        \
-            {"prefix":"supervised",
-            "lambda":0.1,
-            "gamma":0.1,
-            "learning_rate":0.001,
-            "num_steps":50,
-            "num_batches":10001},
-        \
-            {"prefix":"both",
-            "lambda":0.1,
-            "gamma":0.1,
-            "learning_rate":0.001,
-            "num_steps":50,
-            "num_batches":20001}]
+  def __init__(self):
+    self.blocks = [ \
+      {"prefix":"unsupervised",
+      "lambda":0.1,
+      "gamma":0.0,
+      "learning_rate":0.001,
+      "num_steps":50,
+      "num_batches":10001},
+    \
+      {"prefix":"supervised",
+      "lambda":0.1,
+      "gamma":0.1,
+      "learning_rate":0.001,
+      "num_steps":50,
+      "num_batches":10001},
+    \
+      {"prefix":"both",
+      "lambda":0.1,
+      "gamma":1.0,
+      "learning_rate":0.001,
+      "num_steps":50,
+      "num_batches":20001},
+    \
+      {"prefix":"both",
+      "lambda":0.1,
+      "gamma":1.0,
+      "learning_rate":0.0001,
+      "num_steps":50,
+      "num_batches":20001}]
